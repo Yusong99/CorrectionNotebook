62b63261008271f5bcb455700c04d8e80303357b
Yusong99 <yusongxu1999@gmail.com>
2024-03-02 12:24:15 +0800

    testController

---
+++ backend/src/main/java/com/tools/backend/Controller/TestController.java
@@ -0,0 +1,13 @@
+package com.tools.backend.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @RequestMapping("/test")
+    public String requestMethodName() {
+        return "OK";
+    }
+}
